02121072c998ca4dca15678a16f7e74ca30e4ded
Delight Chinecherem <dlghtchenecherem@gmail.com>
2025-09-03 01:36:37 +0100

    Add 6-multi_languages_loop.js script

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+// 6-multi_languages_loop.js
+
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each element
+for (const lang of languages) {
+  console.log(lang);
+}
