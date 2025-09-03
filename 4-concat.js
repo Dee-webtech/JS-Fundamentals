7afd30499c36fe4403e7da482478f65c44f6fc8b
Delight Chinecherem <dlghtchenecherem@gmail.com>
2025-09-03 01:30:24 +0100

    Add 4-concat.js script

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+// 4-concat.js
+
+// Get the arguments after the script name
+const args = process.argv.slice(2);
+
+// Print the two arguments in the format "arg1 is arg2"
+console.log(`${args[0]} is ${args[1]}`);
