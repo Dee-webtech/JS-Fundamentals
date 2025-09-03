03b7ed993dd8bb0ab8bc251b7f75180a9c71e7a0
Delight Chinecherem <dlghtchenecherem@gmail.com>
2025-09-03 01:39:33 +0100

    Add 7-multi_c.js script

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+// 7-multi_c.js
+
+const arg = process.argv[2];
+const count = parseInt(arg);
+
+if (isNaN(count)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log("C is fun");
+  }
+}
